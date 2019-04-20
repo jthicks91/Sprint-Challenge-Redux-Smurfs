c186cb84335390f7bca8ff2e5a1688961ff20a62
jthicks91 <jordanthicks91@gmail.com>
2019-04-20 10:19:24 -0700

    built out action types for MVP.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,23 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from "axios";
 
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_FAILURE = "FETCH_SMURF_FAILURE";
+
+export const ADDING_SMURF_START = "ADDING_SMURF_START";
+export const ADDING_SMURF_SUCCESS = "ADDING_SMURF_SUCCESS";
+export const ADDING_SMURF_FAILURE = "ADDING_SMURF_FAILURE";
+
+// export const UPDATING_SMURF_START = "UPDATING_SMURF_START";
+// export const UPDATING_SMURF_SUCCESS = "UPDATING_SMURF_SUCCESS";
+// export const UPDATING_SMURF_FAILURE = "UPDATING_SMURF_FAILURE";
+
+// export const DELETING_SMURF_START = "DELETING_SMURF_START";
+// export const DELETING_SMURF_SUCCESS = "DELETING_SMURF_SUCCESS";
+// export const DELETING_SMURF_FAILURE = "DELETING_SMURF_FAILURE";
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +29,32 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+const url = "http://localhost:3333/smurfs";
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURF_START });
+  axios
+    .get(url)
+    .then(res => {
+      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_SMURF_FAILURE, payload: err.response });
+    });
+};
+
+export const addSmurf = (smurf = dispatch => {
+  dispatch({ type: ADDING_SMURF_START });
+  return axios
+    .post(url, smurf)
+    .then(res => {
+      dispatch({
+        type: ADDING_SMURF_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({ type: ADDING_SMURF_FAILURE, payload: err.response });
+    });
+});
